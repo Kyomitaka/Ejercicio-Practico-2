56ec8abbbe8866a32aa34a342d8b203db593ca67
Kyomitaka <kiyomi.takahashi.cr@gmail.com>
2024-06-27 20:57:35 -0600

    Add files via upload

---
+++ Ejercicio practico 2/src/ejercicio/practico/pkg2/Cancion.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.practico.pkg2;
+
+/**
+ *
+ * @author kiyomi
+ */
+public class Cancion {
+    public int id = 0;
+    public String titulo = "";
+    public String autor = "";
+    public String duracion = "";
+    private String anio_creacion = "";
+    
+public static void main(String[] args) {
+    
+    Cancion cancion = new Cancion();
+        int id1 = cancion.id;      
+}
+    
+}
